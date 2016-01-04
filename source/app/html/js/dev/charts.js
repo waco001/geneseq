818d2bcbee5030a53cad219bb83be15a78134e2d
Abhishek Gorti <abhishek.gorti@gmail.com>
2016-01-03 20:05:43 -0500

    Move texttip down so horizontal spasm doesnt happen

--- source/app/html/js/dev/charts.js
+++ source/app/html/js/dev/charts.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
         
         bodydonut.plot(human_id, '/human/chart/bodymap', {'node': 'div#bodydonut-chart'});
         
-        $('#bodydonut-chart').append('<span class="texttip">ASDASDASD</span>');
+        $('#bodydonut-chart').append('<br><span class="texttip">ASDASDASD</span>');
     }
 
     if (brainspan != null) {
